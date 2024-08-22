1a62254d63d3ac5e4396c4d56eb9ee5a57eca970
nileshsaple <saplenilesh@yahoo.com>
2024-08-22 23:47:46 +0530

    new files added

---
+++ src/com/collections/list/examples/HashSetExample.java
@@ -0,0 +1,16 @@
+package com.collections.list.examples;
+
+import java.util.*;
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        Set<String> hashSet = new HashSet<>();
+        hashSet.add("Apple");
+        hashSet.add("Banana");
+        hashSet.add("Cherry");
+
+        for(int i = 0; i<hashSet.size(); i++){
+
+        }
+    }
+}
