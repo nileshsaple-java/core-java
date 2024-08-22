aafd917f31b60abb483898160edbee5986e4cfb6
nileshsaple <saplenilesh@yahoo.com>
2024-08-22 07:16:11 +0530

    new files added

---
+++ src/com/strings/Examples/ImmutableClass.java
@@ -0,0 +1,33 @@
+package com.strings.Examples;
+
+public final class ImmutableClass {
+    private final String name;
+    private final int age;
+
+    public ImmutableClass(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+    public static void main(String[] args) {
+        ImmutableClass student = new ImmutableClass("Roshan", 24);
+        student.getName();
+        student.getAge();
+        System.out.println("Name is " + student.getName() + " and he is " + student.getAge() + " years old.");
+
+         ImmutableClass student1 = new ImmutableClass("Sumit", 29);
+         student1.getName();
+         student1.getAge();
+         System.out.println("Name is " + student1.getName() + " and he is " + student1.getAge() + " years old.");
+    }
+}
+
